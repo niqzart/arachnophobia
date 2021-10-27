6db04ebb94303d3039b4a05dd05c8348879f18cf
qzart45hi <qwert45hi@yandex.ru>
2021-10-27 17:19:16 +0300

    feat: persistence for the Point class

---
+++ src/main/java/lab3/Point.java
@@ -0,0 +1,88 @@
+package lab3;
+
+import jakarta.persistence.*;
+
+@Table(name = "point")
+@Entity
+public class Point {
+    private static boolean isInside(double x, double y, int r) {
+        if (x >= r) return false;
+        if (x >= 0) return y >= 0 && y <= r / 2.;
+        return y >= Math.min(-2 * x - 1, 0) && y <= Math.sqrt(r * r - x * x);
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "x", nullable = false)
+    private Double x;
+
+    @Column(name = "y", nullable = false)
+    private Double y;
+
+    @Column(name = "r", nullable = false)
+    private Integer r;
+
+    @Column(name = "inside", nullable = false)
+    private Boolean inside = false;
+
+    @Column(name = "session", nullable = false)
+    private Long session;
+
+    public Point(double x, double y, int r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.inside = isInside(x, y, r);
+    }
+
+    public Long getSession() {
+        return session;
+    }
+
+    public void setSession(Long session) {
+        this.session = session;
+    }
+
+    public Boolean getInside() {
+        return inside;
+    }
+
+    public void setInside(Boolean inside) {
+        this.inside = inside;
+    }
+
+    public Integer getR() {
+        return r;
+    }
+
+    public void setR(Integer r) {
+        this.r = r;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
