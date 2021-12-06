bbf957fe6112430c5eaffc13c6cb5262fba3fc50
qzart45hi <qwert45hi@yandex.ru>
2021-12-07 00:12:00 +0300

    feat(front): add mui

--- front/src/index.js
+++ front/src/index.js
@@ -1,18 +1,20 @@
-import "./styles.css"
-
 import React from "react"
 import { render } from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material"
 
 import HomePage from "./home"
 import MainPage from "./main"
 
 render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/main/" component={MainPage} />
-    </Switch>
-  </BrowserRouter>,
+  <ThemeProvider theme={createTheme({palette: {mode: "dark"}})}>
+    <CssBaseline />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/main/" component={MainPage} />
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById("root")
 )
