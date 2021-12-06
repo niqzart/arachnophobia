be050176d820400097d093e182eb37e50bee505b
qzart45hi <qwert45hi@yandex.ru>
2021-12-06 22:03:05 +0300

    feat(front): add a bit of basic style

--- front/src/home.js
+++ front/src/home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default class HomePage extends Component {
   render() {
     return <div>
-      <h1 style={{ color: "blue" }}>Home</h1>
+      <h1 style={{ color: "cornflowerblue" }}>Home</h1>
       <Link target="/main/" />
     </div>
   }
