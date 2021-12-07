fbc5fa8bc278e2c6d11d4c311a5bd10ce279ce2f
qzart45hi <qwert45hi@yandex.ru>
2021-12-08 00:32:41 +0300

    feat(front): point area & saving points

--- front/src/main.js
+++ front/src/main.js
@@ -22,12 +22,13 @@ class DesktopMainPage extends Component {
       variant="outlined"
       onClick={() => { this.setState({ xError: false, yError: false, rError: false }) }}
       onInput={(event) => {
-        let value = event.target.value.replace(/^-\+\.0-9/gi, "")
-        if (this.treatAsEmpty.includes(value)) {
+        let value = event.target.value.replace(/[^0-9-+.]/gi, "")
+        if (!this.treatAsEmpty.includes(value)) {
           let floatValue = parseFloat(value)
           if (floatValue > maxValue) value = maxValue.toString()
           else if (floatValue < minValue) value = minValue.toString()
         }
+        if (coordName === "r" && value !== this.state.r) this.redrawPoints(value)
         const state = this.state
         state[coordName] = value
         this.setState(state)
@@ -36,14 +37,13 @@ class DesktopMainPage extends Component {
   }
 
   addPoint(x, y) {
-  }
-
-  addPointFromState() {
-    this.addPoint(this.state.x, this.state.y)
+    this.drawPoint({ x, y })
+    const points = this.state.points
+    points.push({ x, y })
+    this.setState({ points })
   }
 
   render() {
-
     return <Grid
       container
       spacing={4}
@@ -71,7 +71,7 @@ class DesktopMainPage extends Component {
           <Grid item xs={3}>
             <Button
               variant="contained"
-              onClick={() => this.addPointFromState()}
+              onClick={() => this.addPoint(this.state.x, this.state.y)}
             >
               Add Point
             </Button>
@@ -79,12 +79,187 @@ class DesktopMainPage extends Component {
         </Grid >
       </Grid>
       <Grid item xs={6} style={{ textAlign: "center" }}>
-        <canvas />
+        <canvas
+          id="point-area"
+          onClick={(event) => {
+            if (this.treatAsEmpty.includes(this.state.r)) {
+              console.log("lol")
+            } else {
+              const canvas = document.getElementById("point-area")
+              const br = canvas.getBoundingClientRect();
+              const x = (event.clientX - br.left - canvas.width / 2) / 130 * this.state.r
+              const y = (-event.clientY + br.top + canvas.height / 2) / 130 * this.state.r
+              this.addPoint(x, y)
+            }
+          }}
+        />
       </Grid>
       <Grid item xs={12} style={{ textAlign: "center" }}>
         Results
       </Grid>
     </Grid>
+  }
+
+  drawPoint({ x, y, r, inside }) {
+    if (r === undefined) r = this.state.r
+    console.log("drawPoint", x, y, r, inside)
+
+    const canvas = document.getElementById("point-area")
+    const context = canvas.getContext("2d")
+    const point_size = 10
+
+    context.beginPath()
+    context.rect(
+      canvas.width / 2 + (x / r * 130) - point_size / 2,
+      canvas.height / 2 - (y / r * 130) - point_size / 2,
+      point_size, point_size
+    )
+    context.closePath()
+
+    console.log(
+      x, y, r,
+      canvas.width / 2 + (x / r * 130) - point_size / 2,
+      canvas.height / 2 - (y / r * 130) - point_size / 2,
+      point_size, point_size
+    )
+
+    context.strokeStyle = "black"
+    context.fillStyle = inside ? "lime" : "red"
+    context.fill()
+    context.stroke()
+  }
+
+  redrawPoints(r) {
+    console.log("hey", r)
+    this.drawPicture(r)
+    for (let point of this.state.points) this.drawPoint({ r, ...point })
+  }
+
+  drawPicture(r) {
+    console.log("fill")
+
+    const canvas = document.getElementById("point-area")
+    const size = 300
+    canvas.width = size
+    canvas.height = size
+
+    const context = canvas.getContext("2d")
+
+    context.clearRect(0, 0, size, size);
+
+    console.log(size)
+
+    // rectangle
+    context.beginPath()
+    context.rect(size / 2, size / 4 + 10, 130, size / 4 - 10)
+    context.closePath()
+    context.strokeStyle = "#2f9aff"
+    context.fillStyle = "#2f9aff"
+    context.fill()
+    context.stroke()
+
+    // sector
+    context.beginPath()
+    context.moveTo(size / 2, size / 2)
+    context.arc(size / 2, size / 2, 130, Math.PI, - Math.PI / 2, false)
+    context.closePath()
+    context.strokeStyle = "#2f9aff"
+    context.fillStyle = "#2f9aff"
+    context.fill()
+    context.stroke()
+
+    // triangle
+    context.beginPath()
+
+    context.moveTo(size / 2, size / 2)
+    context.lineTo(85, size / 2)
+    context.lineTo(size / 2, size - 20)
+    context.lineTo(size / 2, size / 2)
+
+    context.closePath()
+    context.strokeStyle = "#2f9aff"
+    context.fillStyle = "#2f9aff"
+    context.fill()
+    context.stroke()
+
+    // axes
+    context.beginPath()
+    context.font = "10px Verdana"
+    context.strokeStyle = "black"
+    context.fillStyle = "black"
+
+    context.moveTo(size / 2, 0)
+    context.lineTo(size / 2, size)
+
+    context.moveTo(size / 2, 0)
+    context.lineTo(145, 10)
+    context.moveTo(size / 2, 0)
+    context.lineTo(155, 10)
+
+    context.fillText("Y", 160, 8)
+
+    context.moveTo(0, size / 2)
+    context.lineTo(size, size / 2)
+
+    context.moveTo(size, size / 2)
+    context.lineTo(290, 145)
+    context.moveTo(size, size / 2)
+    context.lineTo(290, 155)
+
+    context.fillText("X", 292, 140)
+
+    // Radius-helpers for marks on axes
+    var full_tag = "R"
+    var half_tag = "R/2"
+    var half_shift = true
+    if (r !== "") {
+      full_tag = r
+      half_tag = r / 2
+      half_shift = half_tag % 1
+    }
+
+    // Y marks on axes
+    context.moveTo(145, 20)
+    context.lineTo(155, 20)
+    context.fillText(full_tag, 160, 23)
+
+    context.moveTo(145, 85)
+    context.lineTo(155, 85)
+    context.fillText(half_tag, 160, 87)
+
+    context.moveTo(145, 215)
+    context.lineTo(155, 215)
+    context.fillText("-" + half_tag, 160, 218)
+
+    context.moveTo(145, 280)
+    context.lineTo(155, 280)
+    context.fillText("-" + full_tag, 160, 283)
+
+    // X marks on axes
+    context.moveTo(20, 145)
+    context.lineTo(20, 155)
+    context.fillText("-" + full_tag, 15, 142)
+
+    context.moveTo(85, 145)
+    context.lineTo(85, 155)
+    context.fillText("-" + half_tag, half_shift ? 72 : 78, 142)
+
+    context.moveTo(215, 145)
+    context.lineTo(215, 155)
+    context.fillText(half_tag, half_shift ? 207 : 211, 142)
+
+    context.moveTo(280, 145)
+    context.lineTo(280, 155)
+    context.fillText(full_tag, 277, 142)
+
+    context.closePath()
+    context.strokeStyle = "black"
+    context.fillStyle = "black"
+    context.stroke()
+  }
+
+  componentDidMount() {
+    this.drawPicture("")
   }
 }
 
