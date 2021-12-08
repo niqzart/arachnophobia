3a57b8a132cfba259da74b639ef1f563a4c96270
qzart45hi <qwert45hi@yandex.ru>
2021-12-08 20:07:30 +0300

    feat(back): serving front through spring

--- back/src/main/java/Application.java
+++ back/src/main/java/Application.java
@@ -2,13 +2,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @EnableAutoConfiguration
 public class Application {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @RequestMapping("/main")
+    ModelAndView home() {
+        return new ModelAndView("forward:/");
     }
 
     public static void main(String[] args) {
