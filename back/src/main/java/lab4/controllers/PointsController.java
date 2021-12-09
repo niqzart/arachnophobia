37d6236bb1272ed7c95f2b8e3fc29bcc2e3bb742
qzart45hi <qwert45hi@yandex.ru>
2021-12-09 04:22:50 +0300

    feat(back): utilize user part in PointsController

--- back/src/main/java/lab4/controllers/PointsController.java
+++ back/src/main/java/lab4/controllers/PointsController.java
@@ -1,9 +1,11 @@
 package lab4.controllers;
 
+import lab4.entities.User;
 import lab4.repos.PointRepository;
 import lab4.wrappers.PointWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +20,8 @@ public class PointsController implements WebMvcConfigurer {
 
     @GetMapping("/")
     public ResponseEntity<?> getPointList() {
-        return ResponseEntity.ok().body(repository.findAll().stream().map(PointWrapper::new));
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok().body(repository.findAllByUser(user).stream().map(PointWrapper::new));
     }
 
     @PostMapping(value = "/", consumes = {"application/json"})
@@ -26,7 +29,7 @@ public class PointsController implements WebMvcConfigurer {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body("{\"message\": \"Wrong parameters\"}");
         }
-        repository.save(point.toPoint(null));
+        repository.save(point.toPoint((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
         return ResponseEntity.ok().body("{\"message\": \"OK\"}");
     }
 }
--- back/src/main/java/lab4/repos/PointRepository.java
+++ back/src/main/java/lab4/repos/PointRepository.java
@@ -1,9 +1,13 @@
 package lab4.repos;
 
 import lab4.entities.Point;
+import lab4.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
+    Collection<Point> findAllByUser(User user);
 }
